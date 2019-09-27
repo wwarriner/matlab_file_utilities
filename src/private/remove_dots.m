8568ff582bf6dfee1cf101fdff7027d393020daa
UAB\wwarr <wwarr@uab.edu>
2019-09-27 17:39:54 -0500

    misc fixes

--- src/private/remove_dots.m
+++ src/private/remove_dots.m
@@ -1,6 +1,6 @@
 function dir_table = remove_dots( dir_table )
 
-dots = ismember( { '.', '..' }, dir_table.name );
+dots = ismember( dir_table.name, { '.', '..' } );
 dir_table( dots, : ) = [];
 
 end
